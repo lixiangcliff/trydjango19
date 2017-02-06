51e90e33c209fc75178c3570477a05a1efd0b57b
lixiangcliff <lixiang.cliff@gmail.com>
2017-02-05 23:36:33 -0800

    lesson 16

--- posts/models.py
+++ posts/models.py
@@ -8,8 +8,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
-    updated = models.TimeField(auto_now=True, auto_now_add=False)
-    timestamp = models.TimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
     def __unicode__(self):
         return self.title
--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-
+from .models import Post
 
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
@@ -16,7 +16,9 @@ def post_detail(request):
 
 
 def post_list(request):
+    queryset = Post.objects.all()
     context = {
+        "object_list": queryset,
         "title": "List"
     }
     return render(request, "index.html", context)
