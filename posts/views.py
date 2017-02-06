d84ae1027f9bfbfa293f1ff2f8a9f24f6377f6bf
lixiangcliff <lixiang.cliff@gmail.com>
2017-02-05 23:53:35 -0800

    lesson 17

--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,21 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Post
+
 
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
 
 def post_detail(request):
+    instance = get_object_or_404(Post, id=5)
     context = {
-        "title": "Detail"
+        "title": instance.title,
+        "instance": instance
     }
-    return render(request, "index.html", context)
+    return render(request, "post_detail.html", context)
 
 
 def post_list(request):
