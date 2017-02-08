114aa22fb5d6c9c9333dd85d8ac138c64056ad46
lixiangcliff <lixiang.cliff@gmail.com>
2017-02-08 15:31:39 -0800

    lesson 23

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponsePermanentRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from .models import Post
@@ -58,5 +58,8 @@ def post_update(request, id=None):
     return render(request, "post_form.html", context)
 
 
-def post_delete(request):
-    return HttpResponse("<h1>Delete</h1>")
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Post Successfully Deleted!")
+    return redirect("posts:list")
