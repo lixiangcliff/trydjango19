8a2b8ce52be0f8ba636c44762a77be4b5326bebc
lixiangcliff <lixiang.cliff@gmail.com>
2017-02-09 18:19:45 -0800

    lesson 27

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponsePermanentRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -32,10 +33,22 @@ def post_detail(request, id):
 
 
 def post_list(request):
-    queryset = Post.objects.all()
+    queryset_list = Post.objects.all()  # .order_by('-timestamp')
+    paginator = Paginator(queryset_list, 5)  # Show 25 contacts per page
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
     context = {
         "object_list": queryset,
-        "title": "List"
+        "title": "List",
+        "page_request_var": page_request_var
     }
     return render(request, "post_list.html", context)
 
