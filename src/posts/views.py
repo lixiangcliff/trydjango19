56c41339af1aaaf93835143b589a8e8fa40c22b4
lixiangcliff <lixiang.cliff@gmail.com>
2017-02-11 13:43:11 -0800

    lesson 37

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 from django.http import HttpResponse, HttpResponsePermanentRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -40,9 +41,20 @@ def post_detail(request, slug):
 
 def post_list(request):
     queryset_list = Post.objects.active()  # .order_by('-timestamp')
+    user_can_edit = False
     if request.user.is_staff or request.user.is_superuser:
         queryset_list = Post.objects.all()
-    paginator = Paginator(queryset_list, 5)  # Show 25 contacts per page
+        user_can_edit = True
+
+    query = request.GET.get('q')
+    if query:
+        queryset_list = queryset_list.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(user__first_name__icontains=query) |
+            Q(user__last_name__icontains=query)
+        ).distinct()
+    paginator = Paginator(queryset_list, 2)  # Show 25 contacts per page
     page_request_var = 'page'
     page = request.GET.get(page_request_var)
     try:
@@ -56,7 +68,8 @@ def post_list(request):
     context = {
         "object_list": queryset,
         "title": "List",
-        "page_request_var": page_request_var
+        "page_request_var": page_request_var,
+        "user_can_edit": user_can_edit
     }
     return render(request, "post_list.html", context)
 
@@ -65,7 +78,7 @@ def post_update(request, slug=None):
     if not (request.user.is_staff or request.user.is_superuser):
         raise Http404
     instance = get_object_or_404(Post, slug=slug)
-    form = PostForm(request.POST or None,  request.FILES or None, instance=instance)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
@@ -73,10 +86,12 @@ def post_update(request, slug=None):
         return HttpResponsePermanentRedirect(instance.get_absolute_url())
     elif form.errors:
         messages.error(request, "Post NOT Successfully Updated!")
+    update = True
     context = {
         "title": instance.title,
         "instance": instance,
-        "form": form
+        "form": form,
+        "update": update
     }
     return render(request, "post_form.html", context)
 
